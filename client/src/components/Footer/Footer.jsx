30f0531148c253d989e7c8ce1963e8021e394bde
santivediap <santivediap@gmail.com>
2025-04-09 19:33:25 +0200

    Add credits to BeatSCORE

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return <footer>
-    <Link target="_blank" id="social-instagram" to={ "https://www.instagram.com/beatstar" }></Link>
-    <Link target="_blank" id="social-twitter" to={ "https://x.com/playbeatstar" }></Link>
-    <Link target="_blank" id="social-youtube" to={ "https://www.youtube.com/c/playbeatstar" }></Link>
-    <Link target="_blank" id="social-facebook" to={ "https://www.facebook.com/beatstar" }></Link>
+    <p>Info provided by <Link target="_blank" to={ "https://beatscore.eu/" }>BeatSCORE</Link></p>
+    <div className="socials-container">
+      <Link target="_blank" id="social-instagram" to={ "https://www.instagram.com/beatstar" }></Link>
+      <Link target="_blank" id="social-twitter" to={ "https://x.com/playbeatstar" }></Link>
+      <Link target="_blank" id="social-youtube" to={ "https://www.youtube.com/c/playbeatstar" }></Link>
+      <Link target="_blank" id="social-facebook" to={ "https://www.facebook.com/beatstar" }></Link>
+    </div>
   </footer>;
 };
 
